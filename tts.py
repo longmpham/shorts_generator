f99f4e12f3f89c0daa28654a80e3dbb20c19d187
Long Pham <9040472+longmpham@users.noreply.github.com>
2023-05-28 18:05:16 -0700

    trying new things with tts...

--- tts.py
+++ tts.py
@@ -1,22 +1,24 @@
 from bark import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
-from IPython.display import Audio
 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-from IPython.display import Audio
+from pydub import AudioSegment
 import nltk  # we'll use this to split into sentences
 import numpy as np
 from bark.generation import (
     preload_models,
 )
+from moviepy.editor import *
+
+SUNO_USE_SMALL_MODELS=True # uses 8GB VRAM instead of 12GB recommended
 
 # English Prompt names
 speaker_list = [
     'v2/en_speaker_0',
     'v2/en_speaker_1',
     'v2/en_speaker_2',
-    'v2/en_speaker_3',
+    'v2/en_speaker_3', # EU GUY
     'v2/en_speaker_4',
     'v2/en_speaker_5', # Grainy
     'v2/en_speaker_6', # Suno Favourite
@@ -25,71 +27,56 @@ speaker_list = [
     'v2/en_speaker_9'
 ]
 
-# download and load all models
-preload_models()
 
-# generate audio from text
-# text_prompt = "So my (f16) name is Andi just Andi. \
-#     My moms dad passed away just a few days before \
-#     she found out she was pregnant. My mom was very \
-#     close with her Dad and his name was Andrew \
-#     (also went by Andy). The technical female Version \
-#     of Andrew is Andrea but neither my mom nor dad \
-#     liked the name but my mom wanted to honor her dad \
-#     in some way so I got named Andi. Which I love my name \
-#     I think it fits me. My parents got divorced when I was \
-#     8 and I live with my mom most of the time but visit my \
-#     dad every other weekend (as well as holidays). \
-#     3 years ago my dad started dating his now fiance Kate. \
-#     Kate for some reason when we met assumed my name was \
-#     Andrea. I explained to her it was just Andi. She kept \
-#     calling me Andrea though. I ended up telling my mom about \
-#     it and she told me just to ignore Kate until she calls me \
-#     Andi. Well, this past weekend I was at my dads and we were \
-#     visiting some of Kates family. Well, she kept calling over \
-#     for Andrea and of course, I ignored her. She got mad and said \
-#     why am I ignoring her and I said because that's not my name \
-#     and you know this. Her dad and brother basically laughed \
-#     saying they thought I just went by Andi as a nickname and \
-#     I said no it's just Andi. They then asked Kate why has she \
-#     been calling me Andrea then. Well, Kate later got made calling \
-#     me a brat for embarrassing her. She went on to say I knew who \
-#     she was talking about and that I should have just gone with it \
-#     but I was being an AH. I honestly kind of feel like in that instance \
-#     I should have just answered to Andrea but I don't know. Am I The A-Hole?"
-text_prompt = "So my (f16) name is Andi just Andi. \
-    My moms dad passed away just a few days before \
-    she found out she was pregnant. My mom was very \
-    close with her Dad and his name was Andrew \
-    (also went by Andy)."
+def generate_TTS_using_bark(text_prompt):
+    # download and load all models
+    preload_models()
 
-# split to sentences using nltk
-sentences = nltk.sent_tokenize(text_prompt)
+    # generate audio from text
+    # format should be 2 csv fields so expect a dict of strings. ie. ["what is the ...", "that thing!"]
+    text_prompts = text_prompt if text_prompt else "Do you know this one?"
 
-# generate audio for each sentence
-silence = np.zeros(int(0.25 * SAMPLE_RATE))  # quarter second of silence
-pieces = []
-for i, sentence in enumerate(sentences):
-    audio_array = generate_audio(sentence, history_prompt=speaker_list[6])
-    pieces += [audio_array, silence.copy()]
-    write_wav(f"output_{i}.wav", SAMPLE_RATE, audio_array)
+    # split to sentences using nltk
+    # sentences = nltk.sent_tokenize(text_prompt)
 
+    # generate audio for each sentence
+    silence = np.zeros(int(0.25 * SAMPLE_RATE))  # quarter second of silence
+    pieces = []
+    audio_files = []
+    # for i, sentence in enumerate(sentences):
+    for i, text in enumerate(text_prompts):
+        audio_array = generate_audio(text, history_prompt=speaker_list[3])
+        pieces += [audio_array, silence.copy()]
+        wav_file = write_wav(f"output_{i}.wav", SAMPLE_RATE, audio_array)
+        audio_files.append(wav_file)
+    wav_file = write_wav(f"final_output.wav", SAMPLE_RATE, pieces)
 
-Audio(np.concatenate(pieces), rate=SAMPLE_RATE)
+    
+    # combine audio clips
+    audio1 = AudioSegment.from_file(audio_files[0])
+    audio2 = AudioSegment.from_file(audio_files[1])
+    
+    # Calculate the duration difference for each clip
+    duration_difference_clip1 = 7000 - audio1.duration * 1000  # Difference in milliseconds
+    duration_difference_clip2 = 3000 - audio2.duration * 1000  # Difference in milliseconds
+    
+    # Extend the audio clips by the duration differences
+    extended_clip1 = audio1.fx('loop', n=(duration_difference_clip1 // audio1.duration) + 1)
+    extended_clip2 = audio2.fx('loop', n=(duration_difference_clip2 // audio2.duration) + 1)
+    
+    
+    combined_audio = concatenate_audioclips([extended_clip1, extended_clip2])
 
-# audio_array = generate_audio(text_prompt, speaker_list[6])
-
-# save audio to disk
-# write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
-  
-# play text in notebook
-# Audio(audio_array, rate=SAMPLE_RATE)
+    # create the wav file
+    combined_audio_path = "combined_audio.mp3"
+    combined_audio.write_audiofile(filename=combined_audio_path, fps=extended_clip1.fps, verbose=True, write_logfile=True)
+    
+    
+    # return combined_audio_path
+    return
 
 
-
-
-
-
+generate_TTS_using_bark(["hello world","how are you"])
 
 
 
